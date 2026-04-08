3ddabb250c3995e4126f0905a81acb76ae826e51
aanisawulandarii <aanisawulandarii@gmail.com>
2026-04-08 23:45:51 +0700

    File Upload

---
+++ story_content/triggers.js
@@ -0,0 +1,33 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "6EHuSxCIoho":
+        Script1();
+        break;
+      case "6k2uzN4W1ml":
+        Script2();
+        break;
+      case "5vgr0sieMga":
+        Script3();
+        break;
+      case "65Kj4waChLD":
+        Script4();
+        break;
+      case "6YP5UoIYIWM":
+        Script5();
+        break;
+      case "6HNt9FfwX0z":
+        Script6();
+        break;
+  }
+}
+
+window.InitExecuteScripts = function()
+{
+var player = GetPlayer();
+var object = player.object;
+var addToTimeline = player.addToTimeline;
+var setVar = player.SetVar;
+var getVar = player.GetVar;
+};
